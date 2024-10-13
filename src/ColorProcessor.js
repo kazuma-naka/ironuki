5866f53db538989149989f6ca85fa5da8bb8be3d
unknown <kazumaproject7@gmail.com>
2024-10-13 09:45:48 -0400

    implement 0 ~ 255 color code for Mac terminal

--- src/ColorProcessor.js
+++ src/ColorProcessor.js
@@ -27,7 +27,26 @@ class ColorProcessor {
   }
 
   rgbToColorBlock(r, g, b) {
-    return `\x1b[48;2;${r};${g};${b}m  \x1b[0m`;
+    const isXterm256color = process.env.TERM === "xterm-256color";
+    if (isXterm256color) {
+      const colorCode = this.#rgbToAnsi256(r, g, b);
+      return `\x1b[48;5;${colorCode}m  \x1b[0m`;
+    } else {
+      return `\x1b[48;2;${r};${g};${b}m  \x1b[0m`;
+    }
+  }
+
+  #rgbToAnsi256(r, g, b) {
+    if (r === g && g === b) {
+      if (r < 8) return 16;
+      if (r > 248) return 231;
+      return Math.round(((r - 8) / 247) * 24) + 232;
+    } else {
+      const red = Math.round((r / 255) * 5);
+      const green = Math.round((g / 255) * 5);
+      const blue = Math.round((b / 255) * 5);
+      return 16 + red * 36 + green * 6 + blue;
+    }
   }
 }
 
